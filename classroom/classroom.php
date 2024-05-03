bd2e4d78c7db50e42bfd2c78451289dca568130d
klaus <klaus.andrade@ixcsoft.com.br>
2024-05-03 15:46:31 -0300

    restaurando turmas

--- classroom/classroom.php
+++ classroom/classroom.php
@@ -1,31 +1,54 @@
 <?php
-require_once 'functions-arrays.php';
+require_once 'functions-classroom.php';
 
-    $notas1bi = [
+    $turma1 = [
+        '1' => [
+            "joao" => 8.4,
+            "maria" => 7.2,
+            "luis" => 10,
+            "zeca" => 6,
+            "lucas" => null
+        ],
+        
+        '2' => [
 
-        "joao" => 8.4,
-        "maria" => 7.2,
-        "luis" => 10,
-        "zeca" => 6,
-        "lucas" => null
+            "joao" => 6,
+            "maria" => 9.5,
+            "luis" => 7,
+            "zeca" => 8.4,
+            "lucas" => 10,
+            "marquinhos" => 7.6,
+            "zezinho" => 5.3,
+            "jorlan" => null,
+            "dorival" => 3
+        ]
     ];
-    
-    $notas2bi = [
 
-        "joao" => 6,
-        "maria" => 9.5,
-        "luis" => 7,
-        "zeca" => 8.4,
-        "lucas" => 10,
-        "marquinhos" => 7.6,
-        "zezinho" => 5.3,
-        "jorlan" => null,
-        "dorival" => 3
+    $turma2 = [
+        '1' => [
+            "b" => 8.4,
+            "a" => 7.2,
+            "f" => 10,
+            "d" => 6,
+            "o" => null
+        ],
+        
+        '2' => [
+
+            "b" => 8.4,
+            "a" => 7.2,
+            "f" => 10,
+            "d" => 6,
+            "o" => null,
+            "x" => 7.6,
+            "c" => 5.3,
+            "z" => null,
+            "t"=> 3
+        ]
     ];
 
+    echo "Consulte a turma".PHP_EOL;
+    consultaAluno($turma1 , $turma2);
 
-    echo "aluno matriculado no 1º bimestre?".PHP_EOL;
-    consultaAluno($notas1bi);
-
-    $novatos = viewNovato($notas2bi, $notas1bi);
+    $novatos = viewNovatos($turma1,  $turma2);
     
