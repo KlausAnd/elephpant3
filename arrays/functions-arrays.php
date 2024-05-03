9c635d14e5d26b748a85170f526cb3a114e06411
klaus <klaus.andrade@ixcsoft.com.br>
2024-05-03 14:24:26 -0300

    ver novos alunos

--- arrays/functions-arrays.php
+++ arrays/functions-arrays.php
@@ -1,9 +1,25 @@
 <?php
-    function consultaAluno(array $notas, string $nome){
-        if(array_key_exists($nome, $notas) == false ){
-            echo "$nome nao existe" . PHP_EOL;
+    function consultaAluno(array $notasdobimestre)
+    {
+        $nome = readline('nome: ');
+        if(array_key_exists($nome, $notasdobimestre) == false ){
+            echo "sem matricula encontrada no 1ºbimestre" . PHP_EOL;
         }
         else{
-        echo "$nome existe" . PHP_EOL;
+        echo "matriculado no 1ºbimestre" . PHP_EOL;
         }
+    }
+
+    function viewNovato(array $bimestre1, array $bimestre2)
+    {
+        echo "\tLista de alunos novos: ". PHP_EOL;
+        $alunosNovos = array_diff_key( $bimestre1, $bimestre2 );
+        foreach($alunosNovos as $novatos => $nota){
+            if($nota == null){
+                echo "\t$novatos, não fez prova". PHP_EOL;
+                continue;
+            }
+            echo "\t$novatos, nota $nota".PHP_EOL;
+        }
+        return $alunosNovos;
     }
