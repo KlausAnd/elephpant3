59cb405e8441b26b99dd42fb9027f955cf838cd7
klaus <klaus.andrade@ixcsoft.com.br>
2024-05-15 17:00:45 -0300

    Ajuste caminho de diretórios

--- bancario/banco-oop/banco-oop.php
+++ bancario/banco-oop/banco-oop.php
@@ -1,15 +1,22 @@
 <?php
-    require_once "./src/Conta.php";
-    require_once "./src/Titular.php";
-    require_once "./src/Id.php";
 
-    $titular =  new Titular((new Id($cpf = readline('CPF: '))), $titular = readline('Titular: '));
-    $conta = new Conta($titular);
-    
+require "autoload.php";
+
+use Alura\Banco\Modelo\Conta\Titular;
+use Alura\Banco\Modelo\Endereco;
+use Alura\Banco\Modelo\CPF;
+use Alura\Banco\Modelo\Conta\Conta;
+
+    $endereco = new Endereco(readline('Cidade: '), readline('Rua: '), readline('Numero: '));
+    $titular =  new Titular(new CPF(readline('CPF: ')), readline('Titular: '), $endereco);
+    $conta = new Conta($titular, $endereco);
+
     $conta->deposita(1220);
     $conta->saque(200);
     echo $conta->getSaldo() . PHP_EOL;
 
-    echo Conta::numeroContas() . PHP_EOL;
+    echo $titular->getCPF();
+
+    echo PHP_EOL . Conta::numeroContas() . PHP_EOL;
 
     var_dump($conta);
